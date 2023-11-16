43e204e7c40288461c4f40b509cbe08ab975e6bf
Neal40 <rudyducom@gmail.com>
2023-11-16 17:31:19 +0100

    " Ajout d'un input ID "

--- Todo2/src/main/java/DM_Todo/Todo/TodoApplication.java
+++ Todo2/src/main/java/DM_Todo/Todo/TodoApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,18 +21,6 @@ public class TodoApplication {
 	@Autowired
 	private TodoRepository todoRepository;
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
-
-	@Autowired
-	public void authenticationManager(AuthenticationManagerBuilder builder) throws Exception {
-		builder.inMemoryAuthentication()
-				.withUser("utilisateur")
-				.password("motdepasse")
-				.roles("UTILISATEUR");
-	}
 
 	@Bean
 	public CommandLineRunner setUpBDD(){
